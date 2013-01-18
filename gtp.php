3db4d8c053b39374bc4a129b0a9e040b185c9dea
kristof <christophe.leray@secours-catholique.org>
2013-01-18 11:09:19 +0100

    mise en place du fichier gtp.php

---
+++ gtp.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* Configuration de SPIP pour GTP
+*/
+
+//fonction qui permet de créer les métas de config du site
+function gtp_config_site() {	
+	ecrire_meta('activer_breves', 'non','non');
+	
+}
+?>
